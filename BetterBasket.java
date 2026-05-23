d6e0a33ad7a10e90518f5de47cc373b967f6c3fc
Ushxyuki <116079119+ushxyuki@users.noreply.github.com>
2026-05-24 00:55:31 +0100

    Update BetterBasket.java

--- BetterBasket.java
+++ BetterBasket.java
@@ -1,65 +1,117 @@
+Of course ❤️ here is the full rewritten `BetterBasket.java` with the author changed to **ushxyuki** and the `String` comparison fixed.
+
+```java
 package catalogue;
 
 import java.io.Serializable;
-import java.util.Collections; 
 
 /**
- * Write a description of class BetterBasket here.
- * 
- * @author  Pahalwattage Don Parindiya Ushana
+ * BetterBasket extends Basket and improves product handling.
+ * It combines duplicate products, removes product quantities,
+ * counts products, and calculates the total basket cost.
+ *
+ * @author  ushxyuki
  * @version 1.0
  */
-public class BetterBasket extends Basket implements Serializable
-{
-  private static final long serialVersionUID = 1L; 
-  
+public class BetterBasket extends Basket implements Serializable {
 
-  // You need to add code here 
-  @Override
-  public boolean add(Product product){
+    private static final long serialVersionUID = 1L;
 
-    // check if a simillar product has been already added to the basket and increment the quantity  
-    for( short i =0; i< this.size(); i++){ 
+    /**
+     * Adds a product to the basket.
+     * If the same product already exists, its quantity is increased.
+     *
+     * @param product the product to add
+     * @return true if the product was added or updated
+     */
+    @Override
+    public boolean add(Product product) {
 
-        if(this.get(i).getProductNum() == product.getProductNum()){  
-            product.setQuantity(this.get(i).getQuantity() +product.getQuantity()); 
-            this.set(i, product);
-            return true;
+        if (product == null) {
+            return false;
         }
-      }
 
-    // else append the new product at the end of the basket 
-    return super.add(product); 
-  }
-  
-  // remove entire product specicied if user implicitly stated or when the product count gets below 1
-  public boolean remove(String product_num, boolean entire_product){  
-    for(short i =0; i <this.size(); i++){
-      Product product =this.get(i);
-      if(product.getProductNum() ==product_num){
-        if(entire_product || product.getQuantity() <=1) {this.remove(i); return true;} 
-        this.get(i).setQuantity(product.getQuantity() -1); 
-        return true;
-      }
+        // Check if a similar product has already been added to the basket
+        for (int i = 0; i < this.size(); i++) {
+
+            Product existingProduct = this.get(i);
+
+            if (existingProduct.getProductNum().equals(product.getProductNum())) {
+                existingProduct.setQuantity(
+                        existingProduct.getQuantity() + product.getQuantity()
+                );
+                return true;
+            }
+        }
+
+        // Otherwise, append the new product at the end of the basket
+        return super.add(product);
     }
 
-    return false;
-  }  
+    /**
+     * Removes a product from the basket.
+     * If entireProduct is true, the whole product is removed.
+     * Otherwise, the product quantity is reduced by one.
+     *
+     * @param productNum the product number to remove
+     * @param entireProduct true to remove the entire product
+     * @return true if the product was found and removed or updated
+     */
+    public boolean remove(String productNum, boolean entireProduct) {
 
-  // get the total number of products in the basket 
-  public int count_products(){
-    int count =0;
-    for(Product product:this) {
-      count +=product.getQuantity(); // add the quantity of a product to the count  
+        if (productNum == null) {
+            return false;
+        }
+
+        for (int i = 0; i < this.size(); i++) {
+
+            Product product = this.get(i);
+
+            if (product.getProductNum().equals(productNum)) {
+
+                if (entireProduct || product.getQuantity() <= 1) {
+                    this.remove(i);
+                    return true;
+                }
+
+                product.setQuantity(product.getQuantity() - 1);
+                return true;
+            }
+        }
+
+        return false;
     }
 
-    return count;
-  } 
+    /**
+     * Gets the total number of products in the basket.
+     *
+     * @return total product quantity
+     */
+    public int count_products() {
 
-  // get total cost for products in the cart
-  public double getTotalCost(){
-    double cost =0.0;
-    for(Product product:this) cost +=product.getPrice() *product.getQuantity();
-    return cost;
-  }  
+        int count = 0;
+
+        for (Product product : this) {
+            count += product.getQuantity();
+        }
+
+        return count;
+    }
+
+    /**
+     * Gets the total cost of all products in the basket.
+     *
+     * @return total basket cost
+     */
+    public double getTotalCost() {
+
+        double cost = 0.0;
+
+        for (Product product : this) {
+            cost += product.getPrice() * product.getQuantity();
+        }
+
+        return cost;
+    }
 }
+```
